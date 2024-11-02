e6383b2830d4e4fb9339200ef63f16b057ee1a01
vtjvlad <vtj.vlad@gmail.com>
2024-11-02 21:44:38 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function toggleFlip(event) {
-if (!event.target.classList.contains('add-to-cart') && event.target.tagName !== 'IMG') {
-        event.currentTarget.classList.toggle('flipped');
-    } 
-}
-
-
